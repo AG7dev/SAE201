653a6b8fb1118b89d7789b2ebb9af57d54214a29
Arvin Gobalasamy <arvin.gobalasamy@etu.u-pec.fr>
2026-06-25 00:02:46 +0200

    style: refonte UI page résultats effectifs (carte résumé, tableau, carte graphique, export)

--- static/js/download.js
+++ static/js/download.js
@@ -20,12 +20,9 @@ document.getElementById("chartJsDownloadButton").addEventListener("click", async
 
     const doc = new jsPDF();
 
-    // Debug : vérification du déclenchement du clic
-    console.log("clique détécté");
-
     // Insertion de l'image dans le PDF (position + dimensions)
     doc.addImage(img, "PNG", 10, 10, 190, 100);
 
     // Téléchargement du fichier PDF généré
-    doc.save("test.pdf");
+    doc.save("graphique.pdf");
 });
