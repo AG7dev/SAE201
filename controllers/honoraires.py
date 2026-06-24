9734967f785c158161e3002b085a63daab308eb9
asettouraman <arthy.settouraman@etu.u-pec.fr>
2026-06-24 12:06:45 +0200

    feature(page-honoraire) : regroupement d'honoraires dans accueil

--- controllers/honoraires.py
+++ controllers/honoraires.py
@@ -8,145 +8,51 @@ import requests
 bp_honoraires = Blueprint("honoraires", __name__)
 api = AmeliAPI()
 
-@bp_honoraires.route('/honoraires/tableau', methods=['GET'])
-def afficher_tableau():
+@bp_honoraires.route('/honoraires/dashboard', methods=['GET'])
+def afficher_honoraires():
     annee = request.args.get("annee")
     dept_id = request.args.get("departement_id")
     prof_id = request.args.get("profession_id")
+    region_id = request.args.get("region_id")
     type_honoraire = request.args.get("type_honoraire")
-    
+
     session = Session()
-    prof_list = session.query(ProfessionSante).all()
-    regions = session.query(Region).all()
+    prof_selectionnee = session.get(ProfessionSante, prof_id) if prof_id else None
+    dept = session.get(Departement, dept_id) if dept_id else None
+    region = session.get(Region, region_id) if region_id else None
+    session.close()
 
-    resultats = []
-    donnees_groupees = {} 
-    prof_selectionnee = None
-    dept = None
+    donnees_evolution = []
+    donnees_specialites = []
+    donnees_groupees_evo = {} 
 
-    if prof_id and dept_id: 
-        prof_selectionnee = session.get(ProfessionSante, prof_id)
-        dept = session.get(Departement, dept_id)
+    territoire_code = dept.code if dept else region_id
+    nom_territoire = dept.libelle if dept else (region.libelle if region else "le territoire")
 
-        if prof_selectionnee and dept: 
-            donnees_brutes = api.get_evolution_honoraires(prof_selectionnee.libelle, dept.code, type_honoraire)
-            
-            for d in donnees_brutes:
-                an = d.get('annee')
-                if an not in donnees_groupees:
-                    donnees_groupees[an] = {'total': 0, 'moyen': 0}
+    if territoire_code:
+        if prof_selectionnee:
+            try:
+                donnees_evolution = api.get_evolution_honoraires(prof_selectionnee.libelle, territoire_code, type_honoraire)
                 
-                donnees_groupees[an]['total'] += float(d.get('montant_honoraires') or 0)
-                donnees_groupees[an]['moyen'] += float(d.get('montant_honoraires_moyens') or 0)
+                for d in donnees_evolution:
+                    an = d.get('annee')
+                    if an not in donnees_groupees_evo:
+                        donnees_groupees_evo[an] = {'total': 0, 'moyen': 0}
+                    donnees_groupees_evo[an]['total'] += float(d.get('montant_honoraires') or 0)
+                    donnees_groupees_evo[an]['moyen'] += float(d.get('montant_honoraires_moyens') or 0)
+            except Exception as e:
+                print(f"Erreur API Évolution : {e}")
 
-            resultats = api.get_honoraires(annee, prof_selectionnee.libelle, dept.code, type_honoraire)
+        if annee:
+            try:
+                donnees_specialites = api.get_specialites(annee, territoire_code, type_honoraire)
+            except Exception as e:
+                print(f"Erreur API Spécialités : {e}")
 
-    session.close()
-    
-    return render_template('tableau_honoraires.html',
-                           resultats=resultats, 
-                           donnees_groupees=donnees_groupees, 
-                           professions=prof_list,
+    return render_template('honoraires.html',
+                           donnees_evolution=donnees_evolution,
+                           donnees_groupees_evo=donnees_groupees_evo,
+                           donnees_specialites=donnees_specialites,
                            prof=prof_selectionnee,
-                           regions=regions,
-                           dept=dept,
+                           nom_territoire=nom_territoire,
                            annee=annee)
-
-
-@bp_honoraires.route('/honoraires/graphique', methods=['GET'])
-def afficher_graphique():
-    annee = request.args.get("annee")
-    dept_id = request.args.get("departement_id")
-    prof_id = request.args.get("profession_id")
-    type_honoraire = request.args.get("type_honoraire")
-
-    session = Session()
-    prof_list = session.query(ProfessionSante).all()
-    regions = session.query(Region).all()
-
-    resultats = []
-    donnees_evolution = [] 
-
-    prof_selectionnee = None
-    dept = None
-
-    if prof_id and dept_id: 
-        prof_selectionnee = session.get(ProfessionSante, prof_id)
-        dept = session.get(Departement, dept_id)
-
-        if prof_selectionnee and dept: 
-            resultats = api.get_honoraires(annee, prof_selectionnee.libelle, dept.code, type_honoraire)
-            
-            donnees_evolution = api.get_evolution_honoraires(prof_selectionnee.libelle, dept.code, type_honoraire)
-
-    session.close()
-
-    return render_template('graphique_honoraires.html',
-                            resultats=resultats, 
-                            donnees_evolution=donnees_evolution,
-                            professions=prof_list,
-                            prof=prof_selectionnee,
-                            regions=regions,
-                            dept=dept,
-                            annee=annee)
-
-@bp_honoraires.route('/honoraires/specialites', methods=['GET'])
-def afficher_specialites():
-        region_id = request.args.get('region_id')
-        departement_id = request.args.get('departement_id')
-        annee = request.args.get('annee')
-        type_honoraire = request.args.get('type_honoraire')
-
-        session = Session()
-        regions = session.query(Region).all()
-        
-        dept = None
-        if departement_id:
-            dept = session.get(Departement, departement_id)
-            
-        session.close()
-
-        donnees_specialites = []
-
-        if annee and (region_id or departement_id):
-            territoire = dept.code if dept else region_id
-            try:
-                donnees_specialites = api.get_specialites(annee, territoire, type_honoraire)
-            except Exception as e:
-                print(f"Erreur API : {e}")
-                donnees_specialites = []
-
-        return render_template('specialites_honoraires.html', 
-                            regions=regions, 
-                            donnees_specialites=donnees_specialites)
-
-
-@bp_honoraires.route('/honoraires/specialites/graphique', methods=['GET'])
-def afficher_specialites_graphique():
-        region_id = request.args.get('region_id')
-        departement_id = request.args.get('departement_id')
-        annee = request.args.get('annee')
-        type_honoraire = request.args.get('type_honoraire')
-
-        session = Session()
-        regions = session.query(Region).all()
-        
-        dept = None
-        if departement_id:
-            dept = session.get(Departement, departement_id)
-            
-        session.close()
-
-        donnees_specialites = []
-
-        if annee and (region_id or departement_id):
-            territoire = dept.code if dept else region_id
-            try:
-                donnees_specialites = api.get_specialites(annee, territoire, type_honoraire)
-            except Exception as e:
-                print(f"Erreur API : {e}")
-                donnees_specialites = []
-
-        return render_template('specialites_graphique.html', 
-                            regions=regions, 
-                            donnees_specialites=donnees_specialites)
